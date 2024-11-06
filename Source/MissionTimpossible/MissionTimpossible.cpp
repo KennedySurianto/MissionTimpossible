bc6b278a4a8b985d534d75a5da628eafe5482393
Kennedy Surianto <kennedysrnt@gmail.com>
2024-11-06 20:27:22 +0700

    first commit: basic movement

---
+++ Source/MissionTimpossible/MissionTimpossible.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MissionTimpossible.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MissionTimpossible, "MissionTimpossible" );
